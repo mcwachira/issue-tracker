a7ab490602411157ab276382e6c709d59b8d0dab
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-11-17 10:27:36 +0300

    Styled active link in the navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import {AiFillBug} from 'react-icons/ai'
+import clsx from 'clsx'
+
+const Navbar = () => {
+
+  const currentPath = usePathname()
+
+    const links =[
+        {label:"Dashboard", href:"/"},
+        {label:"Issues", href:"/issues"}
+    ]
+  return (
+   <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center  text-xl'>
+    <Link href="/"> <AiFillBug size={30} /></Link>
+
+    <ul className='flex space-x-6'>
+
+        {
+            links.map((link) => (<Link  className={clsx(link.href === currentPath ? 'text-zinc-900' :"text-zinc-500", " hover:text-zinc-800 transition-colors")}
+             key={link.href}
+              href={link.href}>{
+                link.label}</Link>))
+        }
+
+    </ul>
+   </nav>
+  )
+}
+
+export default Navbar
