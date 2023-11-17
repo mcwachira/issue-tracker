be124c0fdd2e9f747dbe9a451f3dce4c7437526b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-11-17 20:55:59 +0300

    Feature:Added loading skeletons

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,19 +3,17 @@ import prisma from '@/prisma/client'
 import { Button, Table } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
+import delay from 'delay'
+import IssueAction from './IssueAction'
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
+  await delay(2000)
 
   return (
     <div>
         
-        <div className='mb-5'>
-        <Button>
-            <Link href='/issues/new'> New Issue</Link>
-        </Button>
-
-        </div>
+<IssueAction/>
      
 
         <Table.Root variant='surface'>
