d24bd5ee07c3e9d8d5838b240eb783faee54446f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-11-18 03:19:20 +0300

    chore:Added additional loading files

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,26 @@
+import IssuesStatusBadge from '@/components/IssuesStatusBadge'
+import { Heading, Flex, Card , Text, Box} from '@radix-ui/themes'
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import Markdown from 'react-markdown'
 
 const LoadingIssueDetailPage = () => {
-  return (
-    <div>LoadingIssueDetailPage</div>
-  )
+    return (
+        <Box className='max-w-xl'>
+
+
+            <Heading><Skeleton/></Heading>
+            <Flex className='space-x-3' my='2'>
+            <Skeleton/>
+                <Text><Skeleton/></Text>
+            </Flex>
+            <Card className='prose' mt='4'>
+              <Skeleton count={3}/>
+
+            </Card>
+        </Box>
+    )
 }
 
 export default LoadingIssueDetailPage
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,8 +1,22 @@
+import ErrorMessage from '@/components/ErrorMessage'
+import Spinner from '@/components/Spinner'
+import { Callout, TextField, Button , Box} from '@radix-ui/themes'
+import { register } from 'module'
+import error from 'next/error'
 import React from 'react'
+import { Controller } from 'react-hook-form'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingNewIssuePage = () => {
   return (
-    <div>LoadingNewIssuePage</div>
+
+    <Box className='max-w-xl'>
+
+<Skeleton/>
+<Skeleton height='20rem'/>
+
+    </Box>
   )
 }
 
