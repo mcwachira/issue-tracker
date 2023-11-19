201387f3823448602d302b207ee2eb66b5c34f7e
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-11-19 08:37:09 +0300

    Chore:Configured Google Provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,14 @@
 import NextAuth from 'next-auth'
+import GoogleProvider from "next-auth/providers/google";
 
 const handler  =({
 
-    providers:[]
+    providers:[
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+          })
+    ]
 
 })
 
