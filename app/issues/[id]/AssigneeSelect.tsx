7594077eaddfe37fba2d537d0795dc063bcffa86
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-11-21 02:03:42 +0300

    chore:Populate the assignee  select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,27 +1,31 @@
-"use client"
+"use  client"
+import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
+
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+
+    const fetchUsers = async() => {
+      const {data} = await axios.get<User[]>('/api/users')
+      setUsers(data)
+    }
+
+    fetchUsers()
+  }, [])
   return (
-<Select.Root defaultValue="apple">
-  <Select.Trigger />
+<Select.Root>
+  <Select.Trigger placeholder='Assign...' />
   <Select.Content>
     <Select.Group>
-      <Select.Label>Fruits</Select.Label>
-      <Select.Item value="orange">Orange</Select.Item>
-      <Select.Item value="apple">Apple</Select.Item>
-      <Select.Item value="grape" disabled>
-        Grape
-      </Select.Item>
+      <Select.Label>Suggestion</Select.Label>
+      {users.map(user =>  <Select.Item key={user.id} value={user.id} >{user.name}</Select.Item>) }
     </Select.Group>
-    <Select.Separator />
-    <Select.Group>
-      <Select.Label>Vegetables</Select.Label>
-      <Select.Item value="carrot">Carrot</Select.Item>
-      <Select.Item value="potato">Potato</Select.Item>
-    </Select.Group>
-  </Select.Content>
+</Select.Content>
 </Select.Root>
   )
 }
