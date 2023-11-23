bd4d8722e5cbbd5eb52bd316c14418fbc9b3d998
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-11-23 06:01:42 +0300

    chore:Created the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import LatestIssues from '@/components/LatestIssues'
 import Pagination from '@/components/Pagination'
+import { Flex } from '@radix-ui/themes'
 import React from 'react'
 
 const Home = ({searchParams}: {searchParams:{page:string}}) => {
   console.log(searchParams)
   console.log(searchParams.page)
   return (
-    <div>
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
+    <div >
+      <LatestIssues/>
+    {/* <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/> */}
     </div>
   )
 }
---
+++ components/LatestIssues.tsx
@@ -0,0 +1,52 @@
+import prisma from '@/prisma/client'
+import { Avatar, Card, Flex, Heading, Table } from '@radix-ui/themes'
+import Link from 'next/link'
+import React from 'react'
+import { IssuesStatusBadge } from '.'
+
+const LatestIssues = async() => {
+
+    const issues = await prisma.issue.findMany({
+      
+        orderBy:{
+            createdAt:'desc'
+        },
+        take:6,
+        include:{
+            assignToUser:true
+        }
+    })  
+    
+    return (
+        <Card>
+            <Heading size='4' mb='4'>
+
+Latest News
+            </Heading>
+ <Table.Root>
+    <Table.Body>
+
+{issues.map((issue) => <Table.Row key={issue.id}><Table.Cell>
+    <Flex justiyf='between'>
+
+    <Flex direction='column' align='start' gap='3'>
+
+<Link href={`/issues/${issue.id}`}>
+{issue.title}
+</Link>
+<IssuesStatusBadge status={issue.status}/>
+
+
+</Flex>
+
+{issue.assignToUser && (<Avatar src={issue.assignToUser.image!} fallback='?' size='2' radius='full'/>)}
+    </Flex>
+    </Table.Cell></Table.Row>)}
+    </Table.Body>
+ </Table.Root>
+
+ </Card>
+  )
+}
+
+export default LatestIssues
